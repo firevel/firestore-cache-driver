5149e121dcbeb1d47f0f848e99ddeda15a10b69b
Michael Slowik <slowik.michael@gmail.com>
2019-06-23 20:35:51 +0200

    Initial driver

---
+++ src/FirestoreCacheServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Firevel\FirestoreCacheDriver;
+
+use App\Extensions\FirestoreCache;
+use Google\Cloud\Firestore\FirestoreClient;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\ServiceProvider;
+
+class FirestoreCacheServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Cache::extend('firestore', function ($app) {
+            $store = $app['config']['cache.default'];
+            return Cache::repository(
+                new FirestoreCache(
+                    new FirestoreClient,
+                    $app['config']["cache.stores.{$store}.collection"],
+                    $this->app['config']['cache.prefix']
+                )
+            );
+        });
+    }
+}
